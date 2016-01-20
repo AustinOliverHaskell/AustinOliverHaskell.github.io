cab5f6d21b9fd7403444bfe893a6d51cc76199f6
AustinOliverHaskell <ahaskell@carthage.edu>
2016-01-20 14:20:59 -0600

    Sign-Up functionality added

---
+++ login_script.js
@@ -0,0 +1,35 @@
+function login(){
+	
+	// Get Username
+	var userName = $("#usr");
+	
+	// Get pass
+	var userPass = $("#pwd");
+	
+	// Get Firebase
+	var ember = new Firebase("https://jterm2016.firebaseio.com");
+	
+}
+
+function signUp() {
+	
+	// Get Username
+	var userName = $("#newUser").val();
+	
+	// Get pass
+	var userPass = $("#newPassword").val();
+	
+	// Get Email
+	var userEmail = $("#newEmail").val();
+	
+	// Get Firebase
+	var ember = new Firebase("https://jterm2016.firebaseio.com");
+	
+	ember.push({Username: userName, Email: userEmail, Password: userPass, Designs: 0});
+	
+	// Clear Fields
+	$("#newUser").val("");
+	$("#newPassword").val("");
+	$("#newEmail").val("");
+	
+}
