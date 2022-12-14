5934f8ef2e1cbdfdc32ba4f48a7c294b2e5b1355
Haskell <austinoliverhaskell@gmail.com>
2022-12-14 17:08:04 -0600

    Added red color to boids, swapped some elements around so I dont have to change a ton of properties

--- blog/boids.js
+++ blog/boids.js
@@ -1,13 +1,18 @@
-const max_velocity = 20;
+const max_velocity = 5;
 const max_acceleration = 1;
-const sight_range = 60;
+const sight_range = 100;
+const avoidance_range = 60;
+
+const max_avoidance_force = 4;
+const max_cohesian_force = 4; 
 
 class Boid {
-	constructor(position, velocity, acceleration) {
+	constructor(position, velocity, acceleration, color) {
 		this.position     = position;
 		this.velocity     = velocity;
 		this.acceleration = acceleration;
 		this.rotation     = 0;
+		this.color        = color;
 	}
 
 	update(screen_width, screen_height, boid_list) {
@@ -17,37 +22,19 @@ class Boid {
 		var flockmates = this.look_for_flockmates(boid_list); 
 		var average_position_of_flockmates = this.calculate_average_position_of_flockmates(flockmates);
 		var average_heading_of_flockmates  = this.calculate_average_heading_of_flockmates(flockmates);
+		var avoidance_for_flockmates       = this.calculate_avoidance_of_flockmates(flockmates);
 
 		if (flockmates.length != 0)
 		{	
-			this.acceleration.x = (average_position_of_flockmates.x - this.position.x);
-			this.acceleration.y = (average_position_of_flockmates.y - this.position.y);
-
-			if (this.acceleration.x > max_acceleration) {
-				this.acceleration.x = max_acceleration;
-			} else if (this.acceleration.x < -max_acceleration) {
-				this.acceleration.x = -max_acceleration;
-			}
-
-			if (this.acceleration.y > max_acceleration) {
-				this.acceleration.y = max_acceleration; 
-			} else if (this.acceleration.y < -max_acceleration) {
-				this.acceleration.y = -max_acceleration;
-			}
-
-			console.log("acceleration is " + this.acceleration.x + ", " + this.acceleration.y);
+			this.acceleration = this.acceleration.add(average_heading_of_flockmates).add(average_position_of_flockmates).add(avoidance_for_flockmates);
+			//this.acceleration = avoidance_for_flockmates; 
+			this.acceleration = this.acceleration.clamp(max_acceleration);
 		}
 
 		this.position = this.position.add(this.velocity);
 		this.velocity = this.velocity.add(this.acceleration);
 
-		if (this.velocity.x > max_velocity) {
-			this.velocity.x = max_velocity;
-		}
-
-		if (this.velocity.y > max_velocity) {
-			this.velocity.y = max_velocity; 
-		}
+		this.velocity = this.velocity.clamp(max_velocity);
 
 		if (this.position.x > screen_width) {
 			this.position.x -= screen_width;
@@ -72,7 +59,7 @@ class Boid {
 	look_for_flockmates(boid_list) {
 		var flockmates = [];
 		for (var boid of boid_list) {
-			if (boid.position == this.position) {
+			if (boid == this) {
 				continue; 
 			}
 
@@ -91,12 +78,13 @@ class Boid {
 		}
 
 		for (var boid of flockmates) {
-			average_point.x += boid.position.x;
-			average_point.y += boid.position.y;
+			average_point = average_point.add(boid.position);
 		}
 
 		average_point.x = average_point.x / flockmates.length; 
 		average_point.y = average_point.y / flockmates.length;
+
+		average_point = average_point.subtract(this.position).clamp(max_cohesian_force);
 
 		return average_point;
 	}
@@ -109,20 +97,42 @@ class Boid {
 		}
 
 		for (var boid of flockmates) {
-			average_heading += boid.rotation;
+			average_heading = average_heading.add(boid.velocity);
 		}
 
-		average_heading= average_heading / flockmates.length;
+		average_heading.x = average_heading.x / flockmates.length;
+		average_heading.y = average_heading.y / flockmates.length;
 
-		return average_heading;
+		return average_heading.subtract(this.velocity).clamp(max_velocity);
 	}
 
 	calculate_avoidance_of_flockmates(flockmates) {
-		
+		var avoidance_vector = zero_vector(); 
+
+		if (flockmates.length == 0) {
+			return avoidance_vector;
+		}
+
+		for (var boid of flockmates) {
+			var distance_from_boid = this.position.distance_between(boid.position);
+
+			if ( distance_from_boid > avoidance_range) {
+				continue;
+			}
+
+			var strength = distance_from_boid / avoidance_range;
+
+			var difference_vector = this.position.subtract(boid.position);
+			avoidance_vector = avoidance_vector.add(difference_vector);
+		}
+
+		avoidance_vector = avoidance_vector.divide_by_scalar(flockmates.length);
+
+		return avoidance_vector;
 	}
 
-	draw(canvas, color) {
-		draw_boid(canvas, this.position.x, this.position.y, this.rotation, 4, color);
+	draw(canvas) {
+		draw_boid(canvas, this.position.x, this.position.y, this.rotation, 4, this.color);
 	}
 
 	clone() {
@@ -147,12 +157,51 @@ class Vector {
 		return new Vector(this.x + other_vector.x, this.y + other_vector.y);
 	}
 
+	subtract(other_vector) {
+		return new Vector(this.x - other_vector.x, this.y - other_vector.y);
+		//return new Vector(other_vector.x - this.x, other_vector.y - this.y);
+	}
+
+	multiply_by_scalar(scalar) {
+		return new Vector(this.x * scalar, this.y * scalar);
+	}
+
+	divide_by_scalar(scalar) {
+		return new Vector(this.x / scalar, this.y / scalar);
+	}
+
+	inverse() {
+		return new Vector( 1 / this.x, 1 / this.y);
+	}
+
 	distance_between(other_point) {
 		return Math.sqrt(Math.pow(other_point.x - this.x, 2) + Math.pow(other_point.y - this.y, 2));
 	}
 
 	rotate(angle) {
 		return new Vector(Math.abs(Math.cos(angle * this.x) - Math.sin(angle * this.y)), Math.abs(Math.sin(angle * this.x) + Math.cos(angle * this.y)));
+	}
+
+	clone() {
+		return new Vector(this.x, this.y);
+	}
+
+	clamp(max) {
+		var copy = this.clone();
+
+		if (copy.x > max) {
+			copy.x = max;
+		} else if (copy.x < -max) {
+			copy.x = -max;
+		}
+
+		if (copy.y > max) {
+			copy.y = max;
+		} else if (copy.y < -max) {
+			copy.y = -max;
+		}
+
+		return copy;
 	}
 }
 
@@ -177,6 +226,8 @@ function draw_boid(canvas, x, y, rotation, scale, color) {
 
 	boid_points.push(rotate_point(scale_point(new Point(0.5, 0), scale), center_point, rotation));
 	boid_points.push(rotate_point(scale_point(new Point(0, 1), scale), center_point, rotation));
+	boid_points.push(rotate_point(scale_point(new Point(0.5, 0.8), scale), center_point, rotation));
+	boid_points.push(rotate_point(scale_point(new Point(0.5, 0.0), scale), center_point, rotation));
 	boid_points.push(rotate_point(scale_point(new Point(0.5, 0.8), scale), center_point, rotation));
 	boid_points.push(rotate_point(scale_point(new Point(1, 1), scale), center_point, rotation));
 
